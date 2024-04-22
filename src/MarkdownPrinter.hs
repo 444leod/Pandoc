b59cfa1ea3d1b4610af4707ddbcefc728f20410b
leodumont444 <leo.dumont@epitech.eu>
2024-04-22 16:20:47 +0200

    feat(md-printer): start body printer

--- src/MarkdownPrinter.hs
+++ src/MarkdownPrinter.hs
@@ -12,8 +12,10 @@ module MarkdownPrinter (
 import Document
 
 printMarkdown :: Document -> String
-printMarkdown doc = printHeader (_header doc)
-    -- ++ printBody (_body(doc))
+printMarkdown doc = case _body doc of
+  Body contents ->
+    printHeader (_header doc) ++ "\n" ++
+    printBody contents
 
 printHeader :: Header -> String
 printHeader header =
@@ -30,3 +32,26 @@ printAuthor (Just author) = "author: " ++ author ++ "\n"
 printDate :: Maybe String -> String
 printDate Nothing = ""
 printDate (Just date) = "date: " ++ date ++ "\n"
+
+printBody :: [Content] -> String
+printBody = concatMap printContent
+
+printContent :: Content -> String
+printContent (CParagraph (Paragraph paragraphContent)) =
+    printParagraph paragraphContent
+printContent _ = ""
+
+
+printParagraph :: [ParagraphContent] -> String
+printParagraph = concatMap printParagraphContent
+
+printParagraphContent :: ParagraphContent -> String
+printParagraphContent (PTextFormat text) = printTextFormat text ++ "\n\n"
+printParagraphContent _ = ""
+
+printTextFormat :: Format -> String
+printTextFormat (Bold text) = "**" ++ printTextFormat text ++ "**"
+printTextFormat (Italic text) = "*" ++ printTextFormat text ++ "*"
+printTextFormat (Code text) = "`" ++ printTextFormat text ++ "`"
+printTextFormat (FContent text) = text
+
