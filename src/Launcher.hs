8e420c05ceac223cae03053f29219667c31ea755
leodumont444 <leo.dumont@epitech.eu>
2024-04-22 15:06:11 +0200

    feat(launcher): add comments

--- src/Launcher.hs
+++ src/Launcher.hs
@@ -20,15 +20,21 @@ import Document
 import Control.Exception
 import Debug.Trace
 
+{- | Parsable data type
+
+    Represents a the Values that can be parsed
+    ERRORVALUE is used to represent an error
+    UNKNOWNEDVALUE is for when the format is not known and need to be inferred
+-}
 data Parsable =
     JSONVALUE JsonValue |
-    XMLVALUE XMLValue | --tmp XML
+    XMLVALUE XMLValue |
     ERRORVALUE String |
     UNKNOWNEDVALUE Parsable deriving (Show)
 
 {- | launchFile function
 
-    Read a file, parse it and print the result based on the configuration
+    Read a file, and gives it to launchParser
 -}
 launchFile :: VerifiedConf -> IO ()
 launchFile conf = do
@@ -38,6 +44,10 @@ launchFile conf = do
         Just fileContent' -> launchParser conf fileContent'
     return ()
 
+{- | launchParser function
+    
+    Parse the content based on conf and gives the result to launchDocument
+-}
 launchParser :: VerifiedConf -> String -> IO ()
 launchParser conf fileContent = do
     parser <- chooseParser (_iFormat conf)
@@ -46,6 +56,10 @@ launchParser conf fileContent = do
         Just val -> trace (show(fst val)) (launchDocument conf (fst val))
     return ()
 
+{- | launchDocument function
+
+    Convert the parsable to a document and gives it to launchPrinter 
+-}
 launchDocument :: VerifiedConf -> Parsable -> IO ()
 launchDocument conf parsable = do
     maybeDoc <- convertToDocument parsable
@@ -53,6 +67,10 @@ launchDocument conf parsable = do
         Nothing -> myError "Error: cannot convert to document"
         Just doc -> launchPrinter (_oFormat conf) (_iFile conf) doc
 
+{- | launchPrinter function
+
+    Print the document based on the format
+-}
 launchPrinter :: Config.Format -> String -> Document -> IO ()
 launchPrinter JSON outfile doc = print (printJson (documentToJson doc))
 launchPrinter XML outfile doc = print "XML PRINT IS NOT IMPLEMENTED YET"
@@ -70,11 +88,21 @@ chooseParser UNKNOWNED = return (UNKNOWNEDVALUE <$> parseUnknowned)
 chooseParser _ = myError "Error: Format Not supported"
     >> return (return (ERRORVALUE "This will never get executed"))
 
+{- | parseUnknowned function
+
+    Parse the content as a JSON or XML value
+    Return a Parsable
+-}
 parseUnknowned :: Parser Parsable
 parseUnknowned =
     JSONVALUE <$> parseJsonValue <|>
     XMLVALUE <$> parseXMLValue
 
+{- | convertToDocument function
+
+    Convert a Parsable to a Document
+    Return a Maybe Document
+-}
 convertToDocument :: Parsable -> IO (Maybe Document)
 convertToDocument (JSONVALUE x) = return (jsonToDocument x)
 convertToDocument (XMLVALUE _) = return Nothing
