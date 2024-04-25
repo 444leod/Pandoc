16afd14c507d7353249db2a0122c45d05d576b1a
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-04-25 18:52:02 +0200

    feat(XMLToDocument): begin bodyToDoc

--- src/XMLToDocument.hs
+++ src/XMLToDocument.hs
@@ -14,11 +14,11 @@ import XML
 import Debug.Trace
 
 xmlToDocument :: XMLValue -> Maybe Document
-xmlToDocument (XMLValue "document" [] childrens) = do
-    header <- trace (show (head childrens)) (getHeader (head childrens))
-    body <- Just (Body [])
-    return $ Document header body
 xmlToDocument (XMLValue _ _ (_:_:_:_)) = Nothing
+xmlToDocument (XMLValue "document" [] childrens) = do
+    header <- getHeader (head childrens)
+    body <- getBody (last childrens)
+    return $ Document header body
 xmlToDocument _ = Nothing
 
 getHeader :: XMLChild -> Maybe Header
@@ -44,3 +44,32 @@ lookupOptionalXML key childs = lookup key (zip (map getName childs) childs)
 getName :: XMLChild -> String
 getName (XMLNode (XMLValue name _ _)) = name
 getName _ = ""
+
+getBody :: XMLChild -> Maybe Body
+getBody (XMLNode (XMLValue "body" [] childs)) = Body <$> getBodyContent childs
+getBody _ = Nothing
+
+getBodyContent :: [XMLChild] -> Maybe [Content]
+getBodyContent [] = Just []
+getBodyContent (x:xs) = do
+    content <- getContent x
+    contents <- getBodyContent xs
+    return $ content:contents
+
+getContent :: XMLChild -> Maybe Content
+getContent (XMLText text) = Just $ CTextFormat $ FContent text
+getContent (XMLNode (XMLValue "paragraph" [] childs)) = do
+    paragraph <- getParagraph childs
+    return $ CParagraph $ Paragraph paragraph
+getContent _ = Nothing
+
+getParagraph :: [XMLChild] -> Maybe [ParagraphContent]
+getParagraph [] = Just []
+getParagraph (x:xs) = do
+    content <- getParagraphContent x
+    rest <- getParagraph xs
+    return $ content:rest
+
+getParagraphContent :: XMLChild -> Maybe ParagraphContent
+getParagraphContent (XMLText text) = Just $ PTextFormat $ FContent text
+getParagraphContent _ = Nothing
